e0c327439255d2a129232135c3dd324688c76553
Najsar <pat.najsarek@gmail.com>
2020-01-30 16:25:18 +0100

    fix2

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const app = express();
 app.disable('x-powered-by');
 app.set('views', __dirname);
 
-app.use('/', require('./router/main'));
+app.use('/api/', require('./router/main'));
 
 app.listen(config.port, () => func.log("SERVER",`Server running on port: ${config.port}`, 0));
